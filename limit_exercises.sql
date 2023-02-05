1bbda8c0907c77997baf6d52232533136ee5b89d
Herb <herbrholder@gmail.com>
2023-02-05 10:41:05 -0600

    Add:limit

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,7 +1,12 @@
-use employees;
-
-SELECT DISTINCT last_name FROM employees order by last_name desc limit 10;
+USE employees;
 
 
-SELECT emp_no from salaries
-              order by salary desc limit 5;
+        SELECT DISTINCT last_name FROM employees
+        ORDER BY last_name DESC
+        LIMIT 10;
+        SELECT emp_no FROM salaries
+        ORDER BY salary DESC
+        LIMIT 5;
+        SELECT emp_no FROM salaries
+        ORDER BY salary DESC
+        LIMIT 5 OFFSET 45;
